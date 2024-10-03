f036051d0f02eaebbfcd5c04d193a3c8dec7a8fe
Jeremy <75492743+MinhIT9@users.noreply.github.com>
2024-10-04 04:50:22 +0700

    V2

--- config-overrides.js
+++ config-overrides.js
@@ -1,16 +1,21 @@
 const webpack = require('webpack');
 
 module.exports = function override(config) {
+  // Gộp tất cả các fallback vào chung một đối tượng
   config.resolve.fallback = {
     crypto: require.resolve('crypto-browserify'),
     stream: require.resolve('stream-browserify'),
-    buffer: require.resolve('buffer')
+    buffer: require.resolve('buffer'),
+    vm: require.resolve('vm-browserify') // Thêm polyfill cho 'vm'
   };
+
+  // Thêm các plugin cần thiết cho việc sử dụng process và buffer
   config.plugins.push(
     new webpack.ProvidePlugin({
-      process: 'process/browser',
-      Buffer: ['buffer', 'Buffer'],
+      process: 'process/browser', // Polyfill cho 'process'
+      Buffer: ['buffer', 'Buffer'], // Polyfill cho 'Buffer'
     })
   );
+
   return config;
 };
